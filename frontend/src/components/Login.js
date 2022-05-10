6f463e068a212c08b6809cf1c43cdde0684dce3c
Gio-Mgm <gio.mgm@outlook.fr>
2022-05-10 12:13:37 +0200

    clone frontend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Login = () => {
     return (
     <a href={`${process.env.REACT_APP_AUTH_ENDPOINT}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=${process.env.REACT_APP_RESPONSE_TYPE}`}>
