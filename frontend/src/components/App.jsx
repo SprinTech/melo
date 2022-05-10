dce1283dc65627f37db8cd5e7388a819aaa8db6f
Gio-Mgm <gio.mgm@outlook.fr>
2022-05-10 12:08:15 +0200

    clone frontend

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -9,13 +9,13 @@ import { Col, Container, Row, Button } from 'react-bootstrap';
 
 const App = () => {
     const [isLogged, setIsLogged] = useState(false)
-    const [test, setTest] = useState("")
-    const handleOnClick = () => {
-        fetch('http://localhost:5000/api/v1/')
-            .then(response => response.json())
-            .then(data => setTest(data))
+    // const [test, setTest] = useState("")
+    // const handleOnClick = () => {
+    //     fetch('http://localhost:5000/api/v1/')
+    //         .then(response => response.json())
+    //         .then(data => setTest(data))
 
-    }
+    // }
     return (
         <div>
             <NavBarComponent isLogged={isLogged} />
@@ -29,14 +29,14 @@ const App = () => {
                         <Dashboard />
                     </Col>
                 </Row>
-                <Button onClick={handleOnClick}>Click !!!!</Button>
+                {/* <Button onClick={handleOnClick}>Click !!!!</Button> */}
             </Container>
             { !isLogged && <Login />}
-            {test && Object.entries(test).map(([k, v]) => {
+            {/* {test && Object.entries(test).map(([k, v]) => {
                 return (
                     <span>{k} : {v}</span>
                 )
-            })}
+            })} */}
         </div>
     );
 }
