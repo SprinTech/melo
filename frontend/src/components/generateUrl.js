2cafff0b5dc8a0741c7568702b08f6e181b00a2d
Gio-Mgm <gio.mgm@outlook.fr>
2022-05-12 13:51:09 +0200

    commit change connection with backend

---
+++ frontend/src/components/generateUrl.js
@@ -0,0 +1,12 @@
+const getRedirectUrl = (clientID, scopes, redirectUri, showDialog) => {
+    return (
+        'https://accounts.spotify.com/authorize?response_type=token' +
+        `&client_id=${clientID}` +
+        `&scope=${scopes.join('%20')}` +
+        `&redirect_uri=${redirectUri}` +
+        '&show_dialog=' +
+        Boolean(showDialog)
+        )
+    }
+
+export default getRedirectUrl
