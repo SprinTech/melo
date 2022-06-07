44cb81894905c80aee32c2273cf9f55aee6c3238
SprinTech <jvansteenkiste2@gmail.com>
2022-06-07 16:07:14 +0200

    update user crud

--- backend/users/crud.py
+++ backend/users/crud.py
@@ -1,8 +1,10 @@
 import sys
 from sqlalchemy.orm import Session
+from fastapi.responses import JSONResponse
 
 sys.path.append("..")
 import models
+import schemas
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
@@ -13,9 +15,22 @@ def get_user_by_username(db: Session, username: str):
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
-def create_user(db: Session, username: str):
-    db_user = models.User(username=username)
+def create_user(db: Session, user: schemas.UserBase):
+    db_user = models.User(**dict(user))
     db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(models.User).get(user_id)
+    db.delete(db_user)
+    db.commit()
+    return f"User with id {user_id} have been deleted successfully"
+
+def update_user(db: Session, user_id: int, user: schemas.UserBase):
+    db_user = db.query(models.User).get(user_id)
+    db_user.username = user.username
     db.commit()
     db.refresh(db_user)
     return db_user
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,6 @@
 import sys
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from typing import List
 from . import crud
 
@@ -18,12 +18,12 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/users/", response_model=schemas.UserBase)
-def create_user(username: str, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_username(db, username=username)
+@router.post("/users/", response_model=schemas.User, status_code=201)
+def create_user(user: schemas.UserBase, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_username(db, username=user.username)
     if db_user:
         raise HTTPException(status_code=400, detail="Username already registered")
-    return crud.create_user(db=db, username=username)
+    return crud.create_user(db=db, user=user)
 
 @router.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
@@ -32,7 +32,21 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
 @router.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = crud.get_user(db, user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
+@router.delete("/users/{user_id}", status_code=200)
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return crud.delete_user(db, user_id)
+
+@router.put("/users/{user_id}", response_model=schemas.User, status_code=200)
+def update_user(user_id: int, user: schemas.UserBase, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return crud.update_user(db, user_id, user)
